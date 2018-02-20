803c6796a64df2882e4aa9cf80241d83921472e0
Eugene Requilet <Eugene@Requilet.co.za>
2018-02-20 16:23:35 +0200

    Manage Assets

---
+++ Manage.AssetWebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Manage.AssetWebUI.Startup))]
+namespace Manage.AssetWebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
